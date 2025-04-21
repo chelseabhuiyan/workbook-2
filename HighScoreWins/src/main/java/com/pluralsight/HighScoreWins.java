b479f2fbc947dbeb8b9ae1da08979edeebfa9e3b
chelseabhuiyan <bhuiyanfchelsea@gmail.com>
2025-04-20 21:49:03 -0400

    prints the score

--- HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
@@ -23,7 +23,31 @@ public class HighScoreWins {
         String[] teams = teamPart.split(":");       // ["Home", "Visitor"]
         String[] scores = scorePart.split(":");     // ["21", "9"]
 
-        
+        //both arrays should have a length of two now, if not something is wrong
+        if (teams.length != 2 || scores.length != 2) {
+            System.out.println("Invalid input format. Use TeamA:TeamB|ScoreA:ScoreB");
+            return;
+        }
+
+        String team1=teams[0]; //first string stored in the teams array is name of team A
+        String team2=teams[1];  //second string in the teams array is name of Team B
+        int score1 = Integer.parseInt(scores[0].trim()); //first number in the score list is score of team A
+        int score2 = Integer.parseInt(scores[1].trim()); //second number in the score list is socre of team b
+
+        // Determine the winner
+        String winner;
+        if (score1 > score2) {
+            winner = team1;
+        } else if (score2 > score1) {
+            winner = team2;
+        } else {
+            winner = "It's a tie!";
+        }
+
+        //Print the result
+        System.out.print("Winner: "+winner);
+
+
         scanner.close();
     }
 }
